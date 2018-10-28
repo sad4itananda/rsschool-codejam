2154402285d48c9d72b3c63dd305b25606df0752
sad4itananda <4ipej6ij@mail.ru>
2018-10-28 16:15:15 +0300

    docs: add sumOfOther.js

---
+++ sumOfOther.js
@@ -0,0 +1,9 @@
+// const sumOfOther = (arr) => {
+// 	if (arr.length === 0) {
+// 		alert('массив пуст!');
+// 		return;
+// 	};
+// 	let sum = arr.reduce((acc, elem) => acc + elem);
+// 	return arr.map( elem => sum - elem );
+// }
+console.log(44);
